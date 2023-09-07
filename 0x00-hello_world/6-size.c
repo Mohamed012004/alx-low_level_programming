707d235227400542ee504698727d4f0d3427fb1b
Mohamed <you@example.com>
2023-09-07 15:15:44 -0700

    MY first task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program prints with printf functoin.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("Size of a int: %ld byte(s)", sizeof(int));
+	printf("Size of a long: %ld byte(s)", sizeof(long));
+	printf("Size of a long long: %ld byte(s)", sizeof(long long));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
+	return (0);
+}
