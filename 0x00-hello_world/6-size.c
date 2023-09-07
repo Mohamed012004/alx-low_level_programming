386dd8973ae7c86d9e0ea806b1e8049ba09a9efc
Mohamed <you@example.com>
2023-09-07 15:18:10 -0700

    MY first task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	printf("Size of a char: %ld byte(s)", sizeof(char));
 	printf("Size of a int: %ld byte(s)", sizeof(int));
-	printf("Size of a long: %ld byte(s)", sizeof(long));
-	printf("Size of a long long: %ld byte(s)", sizeof(long long));
+	printf("Size of a long int: %ld byte(s)", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long));
 	printf("Size of a float: %ld byte(s)", sizeof(float));
 	return (0);
 }
