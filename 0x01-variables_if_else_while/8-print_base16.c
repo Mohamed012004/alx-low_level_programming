ab4241adcfef9430fb1f7300acd981acae43c7b4
Mohamed <you@example.com>
2023-09-08 14:07:21 -0700

    MY second commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print all
+ *  the numbers of base 16 in lowercase.
+ *
+ * Return: 0 Always (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+	int litter = 97;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+	}
+	while (litter <= 102)
+	{
+		putchar(litter);
+			liter++;
+	}
+	putchar('\n');
+	return (0);
+}
