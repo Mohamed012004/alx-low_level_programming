59eb4d13cc8510985c7a715583407efcfbc606a5
Mohamed <you@example.com>
2023-09-08 13:49:29 -0700

    MY second commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print all single
+ * digit numbers of base 10 from 0 to 9.
+ *
+ * Return: 0 Always (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
