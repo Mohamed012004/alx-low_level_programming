cee670f4d03df8494b291fff2df00a3be798bf41
Mohamed <you@example.com>
2023-09-08 15:23:02 -0700

    MY second commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program to print all
+ *   possible different combinations of two digits.
+ *
+ * Return: 0 Always (success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48 ; i <= 57 ; i++)
+		for (j = 48 ;  <= 57 ; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	return (0);
+}
