534328c9cd5e2a784481e5bf9e48b78c09fbd14e
Mohamed <you@example.com>
2023-09-12 11:30:44 -0700

    MY Third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to print alphabet in lowercase
+ * 10 times using print_alphabet prototype
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int count = 10;
+
+	while (count--)
+	{
+		print_alphabet();
+	}
+}
