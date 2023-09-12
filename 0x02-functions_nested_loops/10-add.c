d255163afc97d60eb00b5aa826815721a58f17ed
Mohamed <you@example.com>
2023-09-12 16:33:36 -0700

    MY Third commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: return a + b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
