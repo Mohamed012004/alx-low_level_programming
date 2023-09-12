84e52f404db25b3d295f5cb4cf4df6f30a139b45
Mohamed <you@example.com>
2023-09-12 16:24:54 -0700

    MY Third commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,34 +9,19 @@ void times_table(void)
 	int t = 9;
 	int i, j;
 
-	while (t--)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
 		{
-			int logic = 0;
-
-			_putchar(48);
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
-			for (j = 0; j < 9; j++)
-			{
-				logic += i;
-				if (logic > 9)
-				{
-					_putchar(logic / 10 + 48);
-					_putchar(logic % 10 + 48);
-				}
-				else
-				_putchar(logic + 48);
-				if (j < 8)
-				{
-				_putchar(',');
+			if (i * j > 9)
+				_putchar((i * j) / 10 + 48);
+			else
 				_putchar(' ');
-				_putchar(' ');
-				}
-			}
-			_putchar('\n');
+			_putchar((i * j) % 10 + 48);
 		}
+			_putchar('\n');
 	}
 }
