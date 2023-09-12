fc43922ea2507e1cc62e0a2da570b004912062e3
Mohamed <you@example.com>
2023-09-12 10:28:01 -0700

    MY Third commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char arr[] = "_putchar";
+
+	for (int i = 0, i < 8, ++i)
+		_putchar(arr[i]);
+	putchar('\n');
+	return (0);
+}
