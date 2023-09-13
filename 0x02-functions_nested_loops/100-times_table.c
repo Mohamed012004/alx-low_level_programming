3e87c858d1ca2a5aca8d2f003fd837696f08ab4c
Mohamed <you@example.com>
2023-09-12 18:34:37 -0700

    MY Third commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,27 +11,29 @@ void print_times_table(int n)
 	int i, j;
 
 	if (n >= 0 && n <= 15)
-	for (i = 0; i <= n; i++)
 	{
-		_putchar(48);
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			if (i * j <= 9)
-				_putchar(' ');
-			if (i * j <= 99)
-				_putchar(' ');
-
-			if (i * j > 99)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
-				_putchar((i * j) / 100 + 48);
-				_putchar(((i * j) / 10) % 10 + 48);
+				_putchar(',');
+				_putchar(' ');
+				if (i * j <= 9)
+					_putchar(' ');
+				if (i * j <= 99)
+					_putchar(' ');
+
+				if (i * j > 99)
+				{
+					_putchar((i * j) / 100 + 48);
+					_putchar(((i * j) / 10) % 10 + 48);
+				}
+				else if (n > 9 && n <= 99)
+					_putchar((i * j) / 10 + 48);
+				_putchar((i * j) % 10 + 48);
 			}
-			else if (n > 9 && n <= 99)
-				_putchar((i * j) / 10 + 48);
-			_putchar((i * j) % 10 + 48);
-		}
 			_putchar('\n');
+		}
 	}
 }
