ce08f72c0a83de5267f63e21b81de3cc7b1eee20
Mohamed <you@example.com>
2023-09-13 11:45:28 -0700

    MY Third commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 	/**
-	 * leap year when it can divided
+	 * leap year when it can be divided
 	 * by 100 and 400 or by 4 evenly
 	*/
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
@@ -25,6 +25,7 @@ void print_remaining_days(int month, int day, int year)
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
+
 	}
 	else
 	{
