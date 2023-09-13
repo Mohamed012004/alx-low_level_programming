4f826caf0b461f781d0c38c0057625755c3926c3
Mohamed <you@example.com>
2023-09-12 19:05:09 -0700

    MY Third commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * sum - sumition all number are multiple of 3 or 5
- * below 1024
+ * main - Entry poin
+ *
+ * Description: sumition all number are multiple of 3 or 5
+ * below 1024 (excluded)
+ * Return: always 0 (success)
 */
 
-void sum(void)
+int main(void)
 {
 	int i, sum = 0;
 
 	for (i = 1; i < 1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
-	printf("%d", sum);
+	printf("%d\n", sum);
+	return (0);
 }
