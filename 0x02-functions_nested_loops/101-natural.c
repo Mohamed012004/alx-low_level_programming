985f506abba7bad1f574f4c7009d47f361b82e4d
Mohamed <you@example.com>
2023-09-12 18:54:16 -0700

    MY Third commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * sum - sumition all number are multiple of 3 or 5
+ * below 1024
+*/
+
+void sum(void)
+{
+	int i, sun = 0;
+
+	for (i = 1; i < 1024; i++)
+		sum += i;
+	printf("%d", sum);
+}
