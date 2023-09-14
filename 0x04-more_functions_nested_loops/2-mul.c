07de5d6941c98c445745bb52f342e58c5d006101
Mohamed <you@example.com>
2023-09-14 08:49:13 -0700

    MY ThirdO commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Muliplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a multiply b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
