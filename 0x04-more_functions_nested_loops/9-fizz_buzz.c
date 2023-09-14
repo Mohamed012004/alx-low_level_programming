ade83a127d681227b22c5d9dcc76ee93cb66c4be
Mohamed <you@example.com>
2023-09-14 11:09:10 -0700

    MY ThirdO commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
@@ -24,5 +24,6 @@ int main(void)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
+	printf("Buzz\n");
 	return (0);
 }
