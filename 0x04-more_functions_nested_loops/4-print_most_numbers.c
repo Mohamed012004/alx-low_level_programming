6922ad4ba4f13ea3b34d9e0a582123804eb32f0c
Mohamed <you@example.com>
2023-09-14 09:06:57 -0700

    MY ThirdO commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints digits from 0 to 9
+ * print_most_numbers - prints digits from 0 to 9
  *		Except 2 and 4.
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
