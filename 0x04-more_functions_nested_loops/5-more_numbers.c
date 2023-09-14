4451196183548c645fdee09c737198941908d187
Mohamed <you@example.com>
2023-09-14 09:21:20 -0700

    MY ThirdO commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,16 @@
 
 void more_numbers(void)
 {
-	int i, t = 14;
+	int i, t = 10;
 
 	while (t--)
 	{
-		for (i = 0; i <= 10; i++)
+		for (i = 0; i <= 14; i++)
+		{
 			if (i > 9)
-				_putchar((i / 10) + '0);
-			_putchar(i + '0');
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
