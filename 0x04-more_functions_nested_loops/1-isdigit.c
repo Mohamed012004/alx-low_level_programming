95957e319c5a5e7c10756b66e26905124ad91a1f
Mohamed <you@example.com>
2023-09-14 08:44:25 -0700

    MY ThirdO commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - chack if the input is a digit from 0 to 9
+ * _isdigit - chack if the input is a digit from 0 to 9
  *
  * @c: integer input
  *
  * Return: always 0 (Success)
 */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
