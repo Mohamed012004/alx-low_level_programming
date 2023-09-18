b6864c301f39d8645a3beed0831a83137e918035
Mohamed <you@example.com>
2023-09-18 09:32:24 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * _strlen - return the length of string.
+ *
+ * @s: character arry (string)
+ *
+ * Return: return length of string
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		++count;
+}
