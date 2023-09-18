4ca395b9dad5e5c99962f89769cc92217bbcdaf6
Mohamed <you@example.com>
2023-09-18 13:06:44 -0700

    MY Fourth commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src to the buffer pointed to by dest
+ * *_strcpy - copies the string pointed to
+ *		by src to the buffer pointed to by dest
  *
  * @src: first pointer
  * @dest: second pointer
  *
- * return string point to by dest
+ *Return: return string point to by dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i, l;
-	for (l = 0; s[l]; l++);
+
+	for (l = 0; src[l]; l++)
+		;
 	++l;
 	for (i = 0 ; i < l; ++i)
 		dest[i] = src[i];
