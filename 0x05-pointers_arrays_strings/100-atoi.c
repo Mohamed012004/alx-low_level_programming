89238131152c77677d26bc127fe91396281518c8
Mohamed <you@example.com>
2023-09-18 14:19:02 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to integer.
+ *
+ * @s: integer pointer
+ *
+ * Return: return an integer exist into a string
+*/
+
+int _atoi(char *s)
+{
+	int i, num = 0, n = 0;
+	bool f = false;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' &&  s[i] <= s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			++n;
+		}
+		else if ((s[i] < '0' || s[i] > '9') && n > 0)
+			break;
+
+		if (n == 1 && i != 0 && s[i - 1] == '-')
+			f = true;
+	}
+	if (f)
+		printf("-");
+	if (!n)
+		return (0);
+	return (num);
+}
