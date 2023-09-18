483287de59ddc22276a949c504aa1ec9bff59548
Mohamed <you@example.com>
2023-09-18 15:37:37 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: generates random valid passwords
+ *		for the program 101-crackme.
+ *
+ * Return: alwys 0 (Success)
+*/
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
