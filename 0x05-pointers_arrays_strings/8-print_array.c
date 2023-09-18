58d4f136d6e5fd0feca1dd80be0eaf41082fff35
Mohamed <you@example.com>
2023-09-18 12:14:17 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array.
+ *
+ * @a: arry variable
+ * @n: operand integer
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0;  < n; i++)
+	{
+		printf("%d", a[i])
+			if (i != n - 1)
+			{
+				printf(", ");
+			}
+	}
+}
