868acd382312b7b175f2f4a807420c2c2066ec14
Mohamed <you@example.com>
2023-09-18 09:58:07 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ *
+ * @s: character arry (string)
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+		++count;
+	while (count > 0)
+	{
+		_putchar(s[count]);
+		--count;
+	}
+	_putchar('\n');
+}
