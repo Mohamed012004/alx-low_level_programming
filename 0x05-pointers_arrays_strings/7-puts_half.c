78115922d2b7fe81f296d776b12f6c8b857d95fe
Mohamed <you@example.com>
2023-09-18 11:38:14 -0700

    MY Fourth commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,10 +13,10 @@ void puts_half(char *str)
 	while (str[len])
 		++len;
 	if (len % 2 == 0)
-		i = len / 2;
+		for (i = len / 2; i < len; i++)
+			_putchar(str[i]);
 	else
-		i = len / 2 + 1;
-	for (i; i < len; i++)
+	for (i = len / 2 + 1; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
