ba4946f82825ed0c194e8798b28f38df25bade85
Mohamed <you@example.com>
2023-09-18 11:00:33 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a input string.
+ *
+ * @s: character arry (string)
+ *
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i, len = 0;
+	char temp;
+
+	while (s[len])
+		len++;
+
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
