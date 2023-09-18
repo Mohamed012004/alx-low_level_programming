bdbc5474381eac144120b4fa75e94e96e74ef69d
Mohamed <you@example.com>
2023-09-18 11:24:50 -0700

    MY Fourth commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of input string.
+ *
+ * @str: char arry (string)
+*/
+
+void puts_half(char *str)
+{
+	int i, len = 0;
+
+	while (str[len])
+		++len;
+	for (i = len / 2; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
