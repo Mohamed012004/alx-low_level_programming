e36eb178f500e333c46715a8b673adb0cc4602a5
Mohamed <you@example.com>
2023-09-20 14:28:32 -0700

    MY FourthO commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all Words of string.
+ *
+ * @s: string
+ *
+ * Return: a string (s)
+*/
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[0] >= 97 && s[0] >= 122)
+		s[0] -= 32;
+	for (i = 1; s[i] != '\0'; i++)
+		if ((s[i] == ' ' || s[i] == '\n') && s[i + 1] >= 97 && s[i + 1] <= 122)
+			s[i + 1] -= 32;
+	return (s);
+}
