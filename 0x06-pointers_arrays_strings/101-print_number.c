8059d2b5b7b43401de75cd7431bc037e095161eb
Mohamed <you@example.com>
2023-09-20 16:57:14 -0700

    MY FourthO commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer number
+ *
+ * @n: operand integer
+ *
+ * Return: void
+*/
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
+}
+
