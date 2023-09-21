a3c4af13bb986f5c09dfb25f924793951ea0e99f
Mohamed <you@example.com>
2023-09-20 17:00:18 -0700

    MY FourthO commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,13 +13,13 @@ void print_number(int n)
 	unsigned int num;
 
 	num = n;
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		num = -n;
 	}
 	if (num / 10 > 0)
 		print_number(num / 10);
-	_putchar(num % 10 + '0');
+	_putchar((num % 10) + '0');
 }
 
