2e0b25ae4f1f36d72329138bac7ef7ee8c16cdd0
Mohamed <you@example.com>
2023-09-25 17:19:38 -0700

    MY Fourth EvenO commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -26,7 +26,7 @@ char *_strstr(char *haystack, char *needle)
 		else
 			k = 0;
 
-		if (k == n && neehl[i] != '\0')
+		if (k == n && needle[i] != '\0')
 			return (haystack + (i - n + 1));
 	}
 	return (NULL);
