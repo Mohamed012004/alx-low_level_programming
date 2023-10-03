373917d63443ffdaaadbac56432b1cc2cf83e404
Mohamed <you@example.com>
2023-10-02 17:58:48 -0700

    bb

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,17 @@
 int
 main(int argc, char const *argv[])
 {
-	long long sum;
+	int sum;
 
 	if (argc == 3)
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf("%lld\n", sum);
+		printf("%d\n", sum);
 	}
 	else
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
