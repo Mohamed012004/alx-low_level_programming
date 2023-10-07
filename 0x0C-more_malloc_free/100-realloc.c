f11d6dae71951e0c3e544b4de13b5be977da3c49
Mohamed <you@example.com>
2023-10-07 08:26:02 -0700

    CC

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == old_size)
 		return (ptr);
-	else if (!new_size && ptr)
+	else if (!new_size && ptr != NULL)
 	{
 		return (free(ptr), NULL);
 	}
