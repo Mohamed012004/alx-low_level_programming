012b4b3df7e9a6d2a322dc66debee707556568c6
Mohamed <melsayed@example.com>
2023-10-24 12:09:40 -0700

    linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * print_listint_len - prints all the elements of a listint_t list.
+ * listint_len - prints all the elements of a listint_t list.
  *
  * @h: pointer of structure.
  *
  * Return: numbers of nodes.
  */
 
-size_t print_listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
 
